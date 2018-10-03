02c8d6aecd6d6a152b815306661ad2a4957d9515
Mateus Rodrigues <mateus.cunhamartins@gmail.com>
2018-10-03 10:02:14 -0300

    Banco de Dados INACABADO
    tirar duvida de como as foreign keys devem ser inseridas  (diretamente no create ou pelo alter table)
    
    qual tipo de dado o material receberá

---
+++ SkeletonBancoDeDados.sql
@@ -0,0 +1,29 @@
+CREATE TABLE cadastro (
+cod_cadastro INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+nome VARCHAR (50),
+senha VARCHAR(60),
+email VARCHAR(50),
+cpf VARCHAR(11),
+escola VARCHAR (50),
+cidade VARCHAR(50),
+estado VARCHAR(50),
+alertasEmail BOOLEAN
+);
+
+CREATE TABLE professor(
+cod_professor INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+/////////////////////////////////
+);
+
+CREATE TABLE conteudo (
+cod_conteudo INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+//////////////////////////
+FK
+FK
+);
+
+
+
+
+
+
