fbce2f6383a37c3c451ec711b67568dec8f6c7b2
andersoooncruz <andersonmarquesdacruzx@gmail.com>
2018-10-10 10:04:17 -0300

    LER COMENTARIO
    feed de materia, barre de menu

--- Mandaí/materia/historia.php
+++ Mandaí/materia/historia.php
@@ -110,26 +110,26 @@ margin: 80px;
       <a class="navbar-brand" href="#">Mandaí</a>
     </div>
     <ul class="nav navbar-nav">
-      <li class="active"><a href="index.php">Início</a></li>
+      <li class="active"><a href="../telaInicial/index.php">Início</a></li>
       <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Matérias<span class="caret"></span></a>
         <ul class="dropdown-menu">
-          <li><a href="../materia/matematica.php">Matemática</a></li>
-          <li><a href="../materia/portugues.php">Português</a></li>
-          <li><a href="../materia/fisica.php">Física</a></li>
-					<li><a href="../materia/historia.php">História</a></li>
-          <li><a href="../materia/geografia.php">Geografia</a></li>
-          <li><a href="../materia/filosofia.php">Filosofia</a></li>
-					<li><a href="../materia/sociologia.php">Sociologia</a></li>
-          <li><a href="../materia/biologia.php">Biologia</a></li>
-          <li><a href="../materia/ingles.php">Inglês</a></li>
-					<li><a href="../materia/quimica.php">Química</a></li>
-					<li><a href="../materia/desenho.php">Desenho</a></li>
-					<li><a href="../materia/musica.php">Música/Arte</a></li>
-					<li><a href="../materia/educacaofisica.php">Educação Física</a></li>
-					<li><a href="../materia/lp.php">Linguagem de Programação</a></li>
-					<li><a href="../materia/icc.php">Introdução a Ciência da Computação</a></li>
-					<li><a href="../materia/bd.php">Banco de Dados</a></li>
-					<li><a href="../materia/engenharia.php">Engenharia de Software</a></li>
+          <li><a href="matematica.php">Matemática</a></li>
+          <li><a href="portugues.php">Português</a></li>
+          <li><a href="fisica.php">Física</a></li>
+					<li><a href="historia.php">História</a></li>
+          <li><a href="geografia.php">Geografia</a></li>
+          <li><a href="filosofia.php">Filosofia</a></li>
+					<li><a href="sociologia.php">Sociologia</a></li>
+          <li><a href="biologia.php">Biologia</a></li>
+          <li><a href="ingles.php">Inglês</a></li>
+					<li><a href="quimica.php">Química</a></li>
+					<li><a href="desenho.php">Desenho</a></li>
+					<li><a href="musica.php">Música/Arte</a></li>
+					<li><a href="educacaofisica.php">Educação Física</a></li>
+					<li><a href="lp.php">Linguagem de Programação</a></li>
+					<li><a href="icc.php">Introdução a Ciência da Computação</a></li>
+					<li><a href="bd.php">Banco de Dados</a></li>
+					<li><a href="engenharia.php">Engenharia de Software</a></li>
         </ul>
       </li>
       <li><a href="../quemsomos.php">Quem somos</a></li>
