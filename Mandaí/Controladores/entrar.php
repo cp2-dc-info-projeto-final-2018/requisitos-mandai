4103f76b963d7cf687fd687e8584870c345d7798
anderson cruz <andersonmarquesdacruzx@gmail.com>
2018-11-16 10:24:03 -0300

    ajeitando login

--- Mandaí/Controladores/entrar.php
+++ Mandaí/Controladores/entrar.php
@@ -29,7 +29,7 @@
 		{
 			$erro = "Nenhum usuário cadastrado com o e-mail informado";
 		}
-	  else if (password_verify($senha, BuscaSenha($senha)) == false){
+	  else if (password_verify($senha, BuscaSenha($senha))){
 			$erro = "A senha está incorreta";
 		}
 	}
