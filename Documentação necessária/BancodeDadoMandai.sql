e8ce23697861151de45dd75eb79e79188700a255
Matheus Inácio <matheusinaciotdg@gmail.com>
2018-12-18 23:27:52 -0200

    bd

--- Documentação necessária/BancodeDadoMandai.sql
+++ Documentação necessária/BancodeDadoMandai.sql
@@ -3,10 +3,6 @@ matricula VARCHAR(9) NOT NULL,
 nome VARCHAR(50) NOT NULL,
 email VARCHAR(100) NOT NULL,
 senha VARCHAR(60) NOT NULL,
-alertasEmail BOOLEAN NOT NULL,
-cpf VARCHAR(11) NOT NULL,
-escola VARCHAR(50) NOT NULL,
-estado VARCHAR(50) NOT NULL,
 cidade VARCHAR (50) NOT NULL,
 tipo INT NOT NULL,
 PRIMARY KEY(matricula)
@@ -17,13 +13,6 @@ CREATE TABLE disciplina(
   nome VARCHAR(50) NOT NULL UNIQUE,
   PRIMARY KEY(id)
 );
-
-CREATE TABLE aluno(
-matricula_aluno VARCHAR(9) NOT NULL,
-PRIMARY KEY(matricula_aluno),
-FOREIGN KEY(matricula_aluno) REFERENCES cadastro(matricula)
-);
-
 CREATE TABLE professor(
 matricula_professor VARCHAR(7) NOT NULL,
 PRIMARY KEY(matricula_professor),
@@ -37,12 +26,4 @@ arquivo VARCHAR(100) NOT NULL,
 matricula_professor VARCHAR(7) NOT NULL,
 PRIMARY KEY(cod_conteudo),
 FOREIGN KEY(matricula_professor) REFERENCES professor(matricula_professor)
-);
-
-CREATE TABLE COMENTARIO (
-cod_comentario INT AUTO_INCREMENT NOT NULL,
-Caracteres VARCHAR(500),
-cod_conteudo INT NOT NULL,
-PRIMARY KEY(cod_comentario),
-FOREIGN KEY(cod_conteudo) REFERENCES conteudo(cod_conteudo)
 );
